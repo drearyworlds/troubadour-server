a16c0fccdf916ed7662bb06239909178606e602e
Jim Craft <jimcraft@live.com>
2021-02-16 16:44:54 -0600

    Fixed string sanitation and start to add db

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
 import { Configuration } from "./config";
+//import mongoose from "mongoose";
 
 const config = new Configuration()
 const app = express();
@@ -19,6 +20,19 @@ const HTTP_CORS_HEADERS_ALLOWED = "Origin, X-Requested-With, Content-Type, Accep
 const HTTP_HEADER_CONTENT_TYPE = "Content-Type";
 const HTTP_HEADER_CONTENT_TYPE_JSON = "application/json";
 const HTTP_HEADER_CONTENT_TYPE_TEXT = "text/plain";
+
+/*
+async function connectToDatabase() {
+    await mongoose.connect("mongodb://localhost/my_database", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    });
+}
+
+connectToDatabase();
+*/
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -43,8 +57,10 @@ app.get("/songdata", (req, res) => {
     res.setHeader(HTTP_HEADER_CONTENT_TYPE, HTTP_HEADER_CONTENT_TYPE_JSON);
     const songListJson = fs.readFileSync(SONGLIST_JSON, ENCODING_UTF8);
     const songList = JSON.parse(songListJson);
-    const songData = songList['songs'].find(song => (song.title = req.query.title && song.artist == req.query.artist)); 
-    res.send(JSON.stringify(songData))
+    const songData = songList["songs"].find(
+        (song: any) => (song.title = req.query.title && song.artist == req.query.artist)
+    );
+    res.send(JSON.stringify(songData));
 });
 
 app.get("/songlyrics", (req, res) => {
@@ -53,22 +69,24 @@ app.get("/songlyrics", (req, res) => {
     console.log(req.query.artist || "reg.query.artist: null");
     console.log(req.query.title || "req.query.title: null");
 
-    let sanitizedArtist: string = req.query.artist.toString().replace(/:|;|"/g, "_");
-    let sanitizedTitle: string = req.query.title.toString().replace(/:|;|"/g, "_");
+    if (req && req.query && req.query.artist && req.query.title) {
+        let sanitizedArtist: string = req.query.artist.toString().replace(/:|;|"/g, "_");
+        let sanitizedTitle: string = req.query.title.toString().replace(/:|;|"/g, "_");
 
-    console.log(sanitizedArtist || "sanitizedArtist: null");
-    console.log(sanitizedTitle || "sanitizedTitle: null");
+        console.log(sanitizedArtist || "sanitizedArtist: null");
+        console.log(sanitizedTitle || "sanitizedTitle: null");
 
-    const songLyricsFileName = `${SONGS_PATH}${sanitizedArtist} - ${sanitizedTitle}${config.getLyricsExtension()}`;
-    console.log(songLyricsFileName || "songLyricsFileName: null");
+        const songLyricsFileName = `${SONGS_PATH}${sanitizedArtist} - ${sanitizedTitle}${config.getLyricsExtension()}`;
+        console.log(songLyricsFileName || "songLyricsFileName: null");
 
-    if (songLyricsFileName == null || !fs.existsSync(songLyricsFileName)) {
-        res.send(`Failed to retrieve song lyrics for ${songLyricsFileName}`)
-        return;
+        if (songLyricsFileName == null || !fs.existsSync(songLyricsFileName)) {
+            res.send(`Failed to retrieve song lyrics for ${songLyricsFileName}`);
+            return;
+        }
+
+        const songLyrics = fs.readFileSync(songLyricsFileName, ENCODING_UTF8);
+        res.send(songLyrics);
     }
-
-    const songLyrics = fs.readFileSync(songLyricsFileName, ENCODING_UTF8);
-    res.send(songLyrics);
 });
 
 app.post("/currentsong/clear", (req, res) => {
@@ -82,6 +100,7 @@ app.post("/currentsong/clear", (req, res) => {
 });
 
 app.post("/currentsong/update", (req, res) => {
+    console.log("/currentsong/update");
     res.setHeader(HTTP_HEADER_CONTENT_TYPE, HTTP_HEADER_CONTENT_TYPE_JSON);
 
     let currentSongText = ``;
